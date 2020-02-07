bc2e0c0559dc03a361408c97c453f226fd02d637
Dmitriy Maksarow <bondspear1989@gmail.com>
2020-02-07 02:30:37 +0200

    ended

---
+++ class_a.php
@@ -0,0 +1,13 @@
+//драсьте! я сервис а ,написанный голимым прогером :) почти ничего не умею
+
+<?php
+
+class class_a
+{
+	public function addText($text)
+	{
+        $new_text = "$text . hello i am servise (a)"
+
+         return $new_text;
+	}
+}
